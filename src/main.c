2a1ee462000269bc593cd5addfb10879da63ce71
gideon <gmarcustrask26@d-e.org>
2023-11-01 16:25:03 -0400

    LETS GOOOOOOOOOO

--- src/main.c
+++ src/main.c
@@ -1,58 +1,136 @@
+#include <stdio.h>
 #include <SDL.h>
-#include "game/game.h"
+#include <SDL_image.h>
+#include <SDL_ttf.h>
 
-int main(int argc, char* argv[]) {
-    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0) {
-        SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+#include <stdbool.h>
+
+TTF_Font *font;
+
+int main(int argc, char *argv[])
+{
+    bool success = true;
+
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+    {
+        fprintf(stderr, "SDL_Init error: %s\n", SDL_GetError());
         return 1;
     }
 
-    // Create a window
-    SDL_Window* window = SDL_CreateWindow("PixelPlatformer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, 0);
-    if (!window) {
-        SDL_Log("Failed to create window: %s", SDL_GetError());
+    if (IMG_Init(IMG_INIT_PNG) == 0)
+    {
+        fprintf(stderr, "IMG_Init error: %s\n", IMG_GetError());
         SDL_Quit();
-        return 2;
+        return 1;
     }
 
-    // Create a renderer
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    if (!renderer) {
-        SDL_Log("Failed to create renderer: %s", SDL_GetError());
+    SDL_Window *window = SDL_CreateWindow("Pixel Platformer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, 0);
+    if (!window)
+    {
+        fprintf(stderr, "SDL_CreateWindow error: %s\n", SDL_GetError());
+        SDL_Quit();
+        IMG_Quit();
+        return 1;
+    }
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer)
+    {
+        fprintf(stderr, "SDL_CreateRenderer error: %s\n", SDL_GetError());
         SDL_DestroyWindow(window);
         SDL_Quit();
-        return 3;
+        IMG_Quit();
+        return 1;
     }
 
-    // Initialize the game
-    Game* game = Game_Create(renderer);
+    SDL_Texture *backgroundTexture = IMG_LoadTexture(renderer, "res/image/sky.png");
+    if (!backgroundTexture)
+    {
+        fprintf(stderr, "IMG_LoadTexture error: %s\n", IMG_GetError());
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        IMG_Quit();
+        return 1;
+    }
 
-    // Game loop
+    SDL_RenderCopy(renderer, backgroundTexture, NULL, NULL);
+
+    // Initialize SDL_ttf
+    if (TTF_Init() == -1)
+    {
+        printf("SDL_ttf could not initialize! SDL_ttf Error: %s\n",
+               TTF_GetError());
+        success = false;
+    }
+
+    // Load font
+    font = TTF_OpenFont("res/font/vermin-vibes.ttf", 24);
+    if (font == NULL)
+    {
+        printf("Failed to load lazy font! SDL_ttf Error: %s\n", TTF_GetError());
+        success = false;
+    }
+
+    SDL_Color textColor = {255, 255, 255};
+    SDL_Surface *textSurface = TTF_RenderText_Solid(font, "Pixel Platformer", textColor);
+    if (!textSurface)
+    {
+        fprintf(stderr, "TTF_RenderText_Solid error: %s\n", TTF_GetError());
+        TTF_CloseFont(font);
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Texture *textTexture = SDL_CreateTextureFromSurface(renderer, textSurface);
+    if (!textTexture)
+    {
+        fprintf(stderr, "SDL_CreateTextureFromSurface error: %s\n", SDL_GetError());
+        SDL_FreeSurface(textSurface);
+        TTF_CloseFont(font);
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Rect textRect;
+    textRect.x = 10;
+    textRect.y = 10;
+    textRect.w = textSurface->w;
+    textRect.h = textSurface->h;
+
+    // Render the text
+    SDL_RenderCopy(renderer, textTexture, NULL, &textRect);
+    SDL_RenderPresent(renderer);
+
+    // Wait for user to close the window
+    SDL_Event e;
     int quit = 0;
-    while (!quit) {
-        // Handle events
-        SDL_Event event;
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
+    while (!quit)
+    {
+        while (SDL_PollEvent(&e))
+        {
+            if (e.type == SDL_QUIT)
+            {
                 quit = 1;
             }
         }
-
-        // Update the game
-        Game_Update(game);
-
-        // Render the game
-        Game_Render(game);
-
-        // Present the renderer
-        SDL_RenderPresent(renderer);
     }
 
-    // Cleanup and quit
-    Game_Destroy(game);
+    SDL_RenderPresent(renderer);
+    SDL_DestroyTexture(backgroundTexture);
+
+    // Clean up and exit
+    SDL_DestroyTexture(textTexture);
+    SDL_FreeSurface(textSurface);
+    TTF_CloseFont(font);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
+    IMG_Quit();
 
     return 0;
 }
